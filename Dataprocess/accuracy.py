eb808c3a7ebd1525c19dac8c9a20bb406ded73a3
ChaunceyCXC <41087527+ChaunceyCXC@users.noreply.github.com>
2020-06-09 20:00:27 -0400

    Update accuracy.py

--- Dataprocess/accuracy.py
+++ Dataprocess/accuracy.py
@@ -6,7 +6,7 @@ import datetime
 
 #root="/home/chauncey/address/252"
 dictfile="/home/chauncey/Result/addressDic.json"
-folder="/home/chauncey/avertical4"
+folder="/home/chauncey/avertical"
 
 
 addressDic={}
